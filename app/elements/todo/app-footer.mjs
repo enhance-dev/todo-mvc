ca71c1867ca05983411b295645274a4d01b754b4
Ryan Bethel <ryan.bethel@gmail.com>
2023-11-07 15:02:12 -0500

    moving styles into components rebase

--- app/elements/todo/app-footer.mjs
+++
@@ -1,9 +0,0 @@
-export default function TodoAppFooter({ html }) {
-  return html`
-    <footer class="info">
-        <p>Double-click to edit a todo</p>
-        <p>Written by the <a href="https://enhance.dev">Enhance Team</a></p>
-        <p>Part of <a href="https://todomvc.com">TodoMVC</a></p>
-    </footer>
-    `
-}
--- app/elements/todo/app.mjs
+++
@@ -1,9 +0,0 @@
-export default function TodoApp({ html }) {
-  return html`
-    <section class="todoapp">
-      <slot name="header"></slot>
-      <slot name="list"></slot>
-      <slot name="footer"></slot>
-    </section>
-    `
-}
